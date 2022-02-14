d37338874e576c3e8e47ab296d2cd30b497211ee
yash-kulkarni2000 <y.kulkarni2000@gmail.com>
2022-02-14 16:53:55 +0530

    second commit

--- app.py
+++ app.py
@@ -1,39 +1,11 @@
 from flask import Flask
-
- 
-
-# Flask constructor takes the name of
-
-# current module (__name__) as argument.
+from waitress import serve
 
 app = Flask(__name__)
 
- 
-
-# The route() function of the Flask class is a decorator,
-
-# which tells the application which URL should call
-
-# the associated function.
-
 @app.route('/')
-
-# ‘/’ URL is bound with hello_world() function.
-
-def hello_world():
-
-    return 'Hello World'
-
- 
-
-# main driver function
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
-
- 
-
-    # run() method of Flask class runs the application
-
-    # on the local development server.
-
-    app.run()
+    serve(app, host='0.0.0.0', port=8080)
